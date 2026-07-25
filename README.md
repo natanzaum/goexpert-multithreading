2de66eac424dc1f158d27a9b8be031ffa21a0af4
natan <natan@DESKTOP-0MHIMNT>
2026-07-25 20:21:28 -0300

    feat: Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# goexpert-multithreading
-Desafio 2 da Pós Go Expert - Trabalhando com Multithreading
+## goexpert-multithreading  
+###  Desafio 2 da Pós Go Expert - Trabalhando com Multithreading  
+
+#### Como rodar o projeto:    
+- Acesse a pasta do projeto  
+- Digite o comando `go run main.go -cep=NUMERO DO CEP`
+
+Exemplo :
+```
+go run main.go -cep=33200000
+```
+
+Exemplo de retorno BrasilCep:  
+```
+2026/07/25 20:18:50 Reposta recebida da BrasilCep!  
+{"cep":"33203580","state":"MG","city":"Vespasiano","neighborhood":"Nova Pampulha 3ª e 4ª Seção","street":"Rua Trinta e Cinco","service":"open-cep"}
+```
+  
+Exemplo de retorno ViaCep:  
+```
+2026/07/25 20:20:28 Reposta recebida da ViaCep!
+{"cep":"33203-580","logradouro":"Rua Trinta e Cinco","complemento":"","unidade":"","bairro":"Nova Pampulha 3ª e 4ª Seção","localidade":"Vespasiano","uf":"MG","estado":"Minas Gerais","regiao":"Sudeste","ibge":"3171204","gia":"","ddd":"31","siafi":"5425"}
+```
---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type BrasilCep struct {
+	Cep          string `json:"cep"`
+	State        string `json:"state"`
+	City         string `json:"city"`
+	Neighborhood string `json:"neighborhood"`
+	Street       string `json:"street"`
+	Service      string `json:"service"`
+}
+
+type ViaCep struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Estado      string `json:"estado"`
+	Regiao      string `json:"regiao"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+	cep := flag.String("cep", "", "CEP a ser consultado")
+	flag.Parse()
+
+	if cep == nil || *cep == "" || len(*cep) != 8 {
+		log.Fatal("CEP não fornecido. Use a flag -cep para fornecer um CEP válido.")
+	}
+
+	ch1 := make(chan BrasilCep)
+	ch2 := make(chan ViaCep)
+	ch3 := make(chan string) //canal criado apra tratar erros de digitação do CEP
+
+	brasilCepAPIUrl := "https://brasilapi.com.br/api/cep/v1/" + *cep
+	viaCepAPIUrl := "http://viacep.com.br/ws/" + *cep + "/json/"
+
+	//faça a requisição para o brasilcep
+	go func(url string) {
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+		if err != nil {
+			log.Println("Error creating request for BrasilCep API")
+			return
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Println("Error fetching BrasilCep API")
+			return
+		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			ch3 <- "Erro ao obter da BrasilCep API, verifique os dados digitados. Erro: " + resp.Status
+			return
+		}
+
+		var brasilCep BrasilCep
+		if err := json.NewDecoder(resp.Body).Decode(&brasilCep); err != nil {
+			log.Println("Error decoding BrasilCep API response")
+			return
+		}
+
+		ch1 <- brasilCep
+	}(brasilCepAPIUrl)
+
+	go func(url string) {
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+		if err != nil {
+			log.Println("Error creating request for ViaCep API")
+			return
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Println("Error fetching ViaCep API")
+			return
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			ch3 <- "Erro ao obter da ViaCep API, verifique os dados digitados. Erro: " + resp.Status
+			return
+		}
+
+		defer resp.Body.Close()
+
+		var viaCep ViaCep
+		if err := json.NewDecoder(resp.Body).Decode(&viaCep); err != nil {
+			log.Println("Error decoding ViaCep API response")
+			return
+		}
+
+		ch2 <- viaCep
+	}(viaCepAPIUrl)
+
+	select {
+	case brasilCep := <-ch1:
+		log.Println("Reposta recebida da BrasilCep!")
+		encoder := json.NewEncoder(os.Stdout)
+		encoder.Encode(brasilCep)
+	case viaCep := <-ch2:
+		log.Println("Reposta recebida da ViaCep!")
+		encoder := json.NewEncoder(os.Stdout)
+		encoder.Encode(viaCep)
+	case erroCep := <-ch3:
+		log.Println(erroCep)
+	case <-time.After(time.Second * 1):
+		log.Println("Timeout ao receber respostas das APIs")
+	}
+}
